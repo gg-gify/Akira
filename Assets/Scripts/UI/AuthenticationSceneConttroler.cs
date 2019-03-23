4746f25691fdd9f0cadae75ee20554fa79edd2e9
Gilmar <gilmar74796@gmail.com>
2019-03-23 18:54:43 -0300

    Creation of UI Controllers
    AuthenticationController
    ChoseCharacterController
    ControllerController

---
+++ Assets/Scripts/UI/AuthenticationSceneConttroler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class AuthenticationSceneConttroler : MonoBehaviour
+{
+
+    [SerializeField] private InputField inputField;
+
+    public void Authenticate()
+    {
+        SceneManager.LoadScene("ChoseCharactes");
+    }
+}
---
+++ Assets/Scripts/UI/ControllerSceneController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ControllerSceneController : MonoBehaviour
+{
+    private PlayerType playerType;
+
+    private void Start()
+    {
+        string playerTypeStr = PlayerPrefs.GetString("playerType");
+        if(playerTypeStr == "SmallPlayer")
+        {
+            playerType = PlayerType.SmallPlayer;
+        }
+        else if (playerTypeStr == "BigPlayer")
+        {
+            playerType = PlayerType.BigPlayer;
+        }
+        else
+        {
+            Debug.LogError("PlayerTyper not set!");
+        }
+    }
+    public void ChoseCharacter(int playerType)
+    {
+        if(playerType == 0)
+        {
+            PlayerPrefs.SetString("playerType", "SmallPlayer");
+        }
+        else
+        {
+            PlayerPrefs.SetString("playerType", "BigPlayer");
+        }
+
+        SceneManager.LoadScene("ControllerScene");
+    }
+}
+
+public enum PlayerType
+{
+    SmallPlayer,
+    BigPlayer
+}
